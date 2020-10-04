af7121e7ce14deea65ad91cd6cfbd708a94ef7a2
ภูมิพัฒน์ เจริญยนต์วัฒนา <nilphumiphat@outlook.co.th>
2020-10-04 23:34:42 +0700

    push code

---
+++ LuaSTD/std.lua
@@ -0,0 +1,16 @@
+-- Copyright 2020 REDSHADOWTH (Phumiphat Joreonyonwhatthana) under MIT Licence.
+-- MIT Licence https://opensource.org/licenses/MIT
+
+
+-- import systen std funtion from c# code.
+_importData = clr.LuaShare.ImportData
+importData = _importData()
+_allImportData = nil
+
+system = { excelConfig = nil, importData = nil, tableTarget = nil }
+
+function system:import(config, data, table)
+    self.excelConfig = config
+    self.importData = data
+    self.tableTarget = table
+end
